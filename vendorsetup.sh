4fa34cf8093d9373c47cbdfe21336528458f1456
Goku <18100664+Vikas-V2@users.noreply.github.com>
2025-10-15 20:46:49 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,9 @@
-git clone git@github.com:LineageOS/android_device_google_gs101.git device/google/gs101 
-git clone git@github.com:LineageOS/android_device_google_gs201.git device/google/gs201
-git clone git@github.com:LineageOS/android_device_google_gs-common.git  device/google/gs-common
-git clone git@github.com:LineageOS/android_device_google_pantah.git device/google/pantah
-git clone git@github.com:TheMuppets/proprietary_vendor_google_lynx.git vendor/google/lynx
-git clone git@github.com:TheMuppets/proprietary_vendor_google_cheetah.git vendor/google/cheetah
-git clone git@github.com:TheMuppets/proprietary_vendor_google_panther.git vendor/google/panther
+git clone -b lineage-23.0  git@github.com:LineageOS/android_device_google_gs101.git device/google/gs101 
+git clone -b lineage-23.0   git@github.com:LineageOS/android_device_google_gs201.git device/google/gs201
+git clone -b lineage-23.0 git@github.com:LineageOS/android_device_google_gs-common.git  device/google/gs-common
+git clone -b lineage-23.0 git@github.com:LineageOS/android_device_google_pantah.git device/google/pantah
+git clone -b lineage-23.0 git@github.com:TheMuppets/proprietary_vendor_google_lynx.git vendor/google/lynx
+git clone -b lineage-23.0 git@github.com:TheMuppets/proprietary_vendor_google_cheetah.git vendor/google/cheetah
+git clone -b lineage-23.0 git@github.com:TheMuppets/proprietary_vendor_google_panther.git vendor/google/panther
 git clone git@github.com:RisingTechOSS-devices/device_google_lynx-kernels_5.10.git  device/google/lynx-kernels/5.10
 git clone https://github.com/RisingTechOSS-devices/device_google_pantah-kernels_5.10  device/google/pantah-kernels/5.10
